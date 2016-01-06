eb0def1a068bb77351bb5bf86bcb4e53797f9738
Michal <bures.michal@hotmail.com>
2016-01-06 16:39:47 +0000

    Info added.

--- index.php
+++ index.php
@@ -1,31 +1,29 @@
 <?php include("header.php"); ?>
     <div class="jumbotron">
-        <h1>Javasctipt puzzle game</h1>
-        <p class="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+        <h1>Cellular</h1>
+        <p class="lead"> A puzzle game where you can experience the unseen. </p>
         <p><a class="btn btn-lg btn-success" href="game.php" role="button">Play</a></p>
     </div>
 
     <div class="row marketing">
         <div class="col-lg-6">
-            <h4>Subheading</h4>
-            <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+            <h4>About</h4>
+            <p> Cellular is a game developed at Charles University, Prague during 2015/16 Computer Games Development course. </p>
 
-            <h4>Subheading</h4>
-            <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
+            <h4>Authors</h4>
+            <p>Matěj Račínský - Main programmer</p>
+            <p>Michal Bureš - Game designer, Programmer</p>
+            <p>Pavel Liška - Graphic design</p>
 
-            <h4>Subheading</h4>
-            <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
         </div>
 
         <div class="col-lg-6">
-            <h4>Subheading</h4>
-            <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+            <h4>Story</h4>
+            <p>You take the role of a scientist helping your favourite cells to dominate the Petri dish!</p>
 
-            <h4>Subheading</h4>
-            <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
+            <h4>Technology</h4>
+            <p>Cellular is developed in Typescript with the help of <a href="http://www.phaser.io/">Phaser</a>.</p>
 
-            <h4>Subheading</h4>
-            <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
         </div>
     </div>
 
