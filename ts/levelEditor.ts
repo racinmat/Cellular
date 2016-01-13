c9d3667568b31b00cf64c4a09925736bda1116a6
Azathoth <memnarch@seznam.cz>
2016-01-13 18:37:20 +0100

    přidán ts soubor

---
+++ ts/levelEditor.ts
@@ -0,0 +1,59 @@
+/// <reference path="references.ts"/>
+
+class Square {
+	public number : number;
+	public squareType : FloodTactics.SquareType;
+}
+
+var select : JQuery = $('#level');
+for (var level in games)  {
+	var option : HTMLOptionElement = document.createElement('option');
+	option.text = level;
+	option.value = level;
+	select.append(option);
+}
+
+var editedGrid : Square[][] = [];
+var selectedLevel : string;
+
+select.change(function() {
+	selectedLevel = $(this).find(":selected").val();
+	editedGrid = games[selectedLevel].squares;
+
+	var i : number = 0;
+	for (var row of editedGrid) {
+		var j : number = 0;
+		var div : HTMLDivElement = document.createElement("div");
+		$('#content').append(div); // put it into the DOM
+		for (var element of row) {
+			var input : HTMLInputElement = document.createElement("input");
+			input.type = "text";
+			input.style.width = String(50);
+			input.style.height = String(50);
+			input.value = element.squareType.color;
+			input.id = String(i) + '-' + String(j);
+			div.appendChild(input);
+			j++;
+		}
+		i++;
+	}
+});
+
+$('#saveLevel').click(() => {
+	var i : number = 0;
+	for (var row of editedGrid) {
+		var j : number = 0;
+		var div : HTMLDivElement = document.createElement("div");
+		$('#content').append(div); // put it into the DOM
+		for (var element of row) {
+			var input = $('#' + String(i) + '-' + String(j));
+			element.squareType.color = parseInt(input.val());
+			j++;
+		}
+		i++;
+	}
+	games[selectedLevel].squares = editedGrid;
+	FloodTactics.Utils.download(JSON.stringify(games[selectedLevel], null, 4));
+});
+
+
